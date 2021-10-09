1cd3064a6215f237e2f59ae759956fb741e3f42c
krupy <krupy@positron-it.ru>
2021-10-10 00:03:19 +0300

    Made REST API based on gRPC with Gateway

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.16
 
 require (
 	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.6.0
+	google.golang.org/genproto v0.0.0-20210903162649-d08c68adba83
 	google.golang.org/grpc v1.41.0
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0
+	google.golang.org/protobuf v1.27.1
 )
--- server/server.go
+++ server/server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	pb "grpc/gen/proto"
 	"log"
 	"net"
+	"net/http"
 )
 
 type testApiServer struct {
@@ -21,6 +23,17 @@ func (s *testApiServer) Echo(ctx context.Context, req *pb.ResponseRequest) (*pb.
 }
 
 func main() {
+	go func() {
+		// mux
+		mux := runtime.NewServeMux()
+
+		// register
+		pb.RegisterTestApiHandlerServer(context.Background(), mux, &testApiServer{})
+
+		// http server
+		log.Fatalln(http.ListenAndServe("localhost:8081", mux))
+	}()
+
 	lister, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
 		log.Fatalln(err)
